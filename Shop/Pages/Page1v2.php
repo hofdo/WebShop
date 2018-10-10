4fc55214aaeb05c4b12b4d39d6631f0855c99df2
Jan <jan@struebin.com>
2018-10-10 12:47:54 +0200

    Alpha V0.1

--- Shop/Pages/Page1v2.php
+++ Shop/Pages/Page1v2.php
@@ -10,6 +10,9 @@
 <?php include("Shared/Navigation.php") ?>
 
 <aside>
+
+
+
     <h2>Info</h2>
     <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exercitation ulliam corper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
     <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exercitation ulliam corper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
