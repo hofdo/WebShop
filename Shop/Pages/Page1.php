de3b39a01cb69d61848c270760663ca27ef93fa3
hofmd2 <hofmann.dominique@bluewin.ch>
2018-10-16 14:46:39 +0200

    Navigation v0.2 Footer v0.1 Content v0.01 Header v0.1
    Begin of Login

---
+++ Shop/Pages/Page1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Layout Example</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<?php include ("Header.php") ?>
+<?php include ("Navigation.php") ?>
+<?php include ("Content.php") ?>
+<?php include ("Footer.php") ?>
+</body>
+</html>
