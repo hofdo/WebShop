5a50f9816e75969dc7f6c12d9ea93ac9186a13be
hofmd2 <hofmann.dominique@bluewin.ch>
2018-10-16 14:46:39 +0200

    Navigation v0.2 Footer v0.1 Content v0.01 Header v0.1
    Begin of Login

---
+++ Shop/Pages/Footer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: hofma
+ * Date: 10.10.2018
+ * Time: 14:10
+ */
+?>
+
+<footer>
+    <ul class="footerMenu">
+        <li><a>Impressum</a></li>
+        <li><a>Kontakt</a></li>
+        <li><a>Cookies</a></li>
+        <li><a>FAQ</a></li>
+    </ul>
+</footer>
