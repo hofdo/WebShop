d25979666a56eff3cfd53ecb1b0cd5eed6421ba0
hofmd2 <hofmann.dominique@bluewin.ch>
2018-10-17 13:19:10 +0200

    Merge one ready to go boy
    One-sama "kya"

--- Shop/Pages/Footer.php
+++ Shop/Pages/Footer.php
@@ -1,12 +1,3 @@
-<?php
-/**
- * Created by IntelliJ IDEA.
- * User: hofma
- * Date: 10.10.2018
- * Time: 14:10
- */
-?>
-
 <footer>
     <ul class="footerMenu">
         <li><a>Impressum</a></li>
---
+++ Shop/Pages/Login.php
@@ -0,0 +1,13 @@
+<div id="login" class="login_container">
+    <form class="login_Form">
+        <label><b>Username</b></label>
+        <input type="text" placeholder="Username...">
+        <label><b>Username</b></label>
+        <input type="password" placeholder="Password...">
+        <label class="close" onclick="document.getElementById('login').style.display='none'">X</label>
+
+        <button type="submit">Submit</button>
+        <button type="button" onclick="document.getElementById('login').style.display='none'" class="cancel_Btn">Cancel</button>
+    </form>
+
+</div>
