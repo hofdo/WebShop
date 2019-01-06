ca6f33f809a6a1167d008876d4e3df1e10096050
hofmd2 <hofmann.dominique@bluewin.ch>
2019-01-06 14:02:36 +0100

    YEET

--- Shop/Entity/Cart.php
+++ Shop/Entity/Cart.php
@@ -60,7 +60,7 @@ class Cart {
         }
     }
 
-    public function render() {
+    public static function render() {
         $totalValue = 0;
         if (self::isEmpty() || !Product::checkOrderIsOpen()) {
             echo "<div class=\"cart empty\"><table id='shoppingCartTable'><tr><th>Article-Id</th><th>Name</th><th>Value</th><th>Quantity</th></tr><tr><td>Total: </td><td id='totalProductValue'>$totalValue</td></tr></table><div id='cartIsEmpty'>Cart is empty</div></div>";
