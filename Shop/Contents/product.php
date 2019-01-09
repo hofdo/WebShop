a53ae8dab757144f09b0d91088dca9c4435d6e53
hofmd2 <hofmann.dominique@bluewin.ch>
2019-01-09 10:29:20 +0100

    YEET

--- Shop/Contents/product.php
+++ Shop/Contents/product.php
@@ -9,19 +9,18 @@ $product = $product[0];
 
 <section>
     <table>
-        <div class='product_chosen'>
-            <div> <?php
+            <tr><td>
+                <?php
                 echo '<img src="data:picture/jpeg;base64,' . base64_encode($product[4]) . '"height="120" width="120"/>';
-                ?></div>
-            <div class='productTitle'><?php echo t($product[1]) ?></div>
-            <div class='productPrice'><?php echo $product[3] . " sfr" ?></div>
-            <div class='productDescription'><?php echo t($product[1]."_Description") ?></div>
+                ?></td></tr>
+            <tr class="productID"><td <?php echo "id='productID$product[0]'" ?> ><?php echo t($product[0]) ?> </td></tr>
+            <tr class='productTitle'><td <?php echo "id='productTitle_$product[0]'" ?> ><?php echo t($product[1]) ?></td></tr>
+            <tr class='productPrice'><td <?php echo "id='productPrice_$product[0]'" ?> ><?php echo $product[3] . " sfr" ?></td></tr>
+            <tr class='productDescription'><td><?php echo t($product[1] . "_Description") ?></td></tr>
             <?php
             if ($_SESSION["logged_in"]) {
-                printf("<tr><td class='ProductAdd'><button class='buttonAdd' type='submit' onclick='addToShoppingCart(\"%s\")'>" . t("addCart") . "</button></td></tr></table></div>", $product[0]);
+                printf("<tr><td class='ProductAdd'><button class='buttonAdd' type='submit' onclick='addToShoppingCart(\"%s\")'>" . t("addCart") . "</button></td></tr>", $product[0]);
             }
             ?>
-        </div>
     </table>
 </section>
-
