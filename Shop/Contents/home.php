00d663a315c975397c8ef9b046714c94aaefbd11
hofmd2 <hofmann.dominique@bluewin.ch>
2019-01-09 03:41:52 +0100

    YEET

--- Shop/Contents/home.php
+++ Shop/Contents/home.php
@@ -1,6 +1,20 @@
 
 <div class="home_Box">
+    <img src="../Pictures/adventure-backlit-birds-443356.jpg">
+    <p>
 <?php
 echo t("home_1");
 ?>
+    </p>
+    <div>
+        <p class="Zitat">
+            “Life without color is colorless”
+        </p>
+        <p>
+            Some important person ca. 2019
+        </p>
+    </div>
+    <p>
+        Please give us your Money. Thanks. We need it. Really.
+    </p>
 </div>
